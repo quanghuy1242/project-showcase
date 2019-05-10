c42394bbfeda46e770acd9a54464e80e1cd13fa9
quanghuy1242 <katygaga124@gmail.com>
2019-05-10 22:29:18 +0700

    added: search component

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -3,6 +3,7 @@ import { Text } from 'office-ui-fabric-react/lib/Text';
 import { IconButton } from 'office-ui-fabric-react/lib/Button';
 import { Depths } from '@uifabric/fluent-theme/lib/fluent/FluentDepths';
 import NavLink from '../NavLink/NavLink';
+import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
 import './TopNav.css'
 
 class TopNav extends Component {
@@ -34,6 +35,10 @@ class TopNav extends Component {
             </NavLink>
           ))
         }
+        </div>
+        <div className="spacer"></div>
+        <div style={{width: 300, justifySelf: 'flex-end'}} className="SearchWrapper">
+          <SearchBox placeholder="Search" />
         </div>
       </div>
     );
