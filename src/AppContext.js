481a499c5589d91b0ff078dcc0a6c12091e61c31
quanghuy1242 <katygaga124@gmail.com>
2019-05-13 12:17:16 +0700

    removed: AppContext.js from '/src'

--- src/AppContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const LocalUser = {
-  name: 'Quang Huy',
-  image: 'https://raw.githubusercontent.com/quanghuy1242/MyLibary/master/images/cover.jpg',
-  shortName: 'QH'
-}
-
-export const AppContext = React.createContext(LocalUser);
