f15c428943ecf7a4b7d9c235b39551ae337d408a
quanghuy1242 <katygaga124@gmail.com>
2019-05-14 08:49:00 +0700

    added: getProject by Id api

--- src/api/projects.api.js
+++ src/api/projects.api.js
@@ -5,4 +5,9 @@ export class ProjectAPI {
     let projectRespone = await api.get('/projects');
     return projectRespone.data.projects;
   }
+
+  static async getProject(projectsId) {
+    let projectRespone = await api.get(`/projects/${projectsId}`);
+    return projectRespone.data.project;
+  }
 }
