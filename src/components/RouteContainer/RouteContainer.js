0cf21e4855a9a8302da695421b90588843b42b5d
quanghuy1242 <katygaga124@gmail.com>
2019-05-18 15:44:14 +0700

    added: new route /categories

--- src/components/RouteContainer/RouteContainer.js
+++ src/components/RouteContainer/RouteContainer.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../../page/Home/Home';
-import Banner from '../Banner/Banner';
 import NotFound from '../../page/NotFound/NotFound';
 import About from '../../page/About/About';
 import ProjectDetail from '../../page/ProjectDetail/ProjectDetail';
+import Categories from '../../page/Categories/Categories';
 
 class RouteContainer extends Component {
   render() {
@@ -12,7 +12,7 @@ class RouteContainer extends Component {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/project/:id" component={ProjectDetail} />
-        <Route path="/categories" component={Banner} />
+        <Route path="/categories" component={Categories} />
         <Route path="/about" component={About} />
         <Route component={NotFound} />
       </Switch>
