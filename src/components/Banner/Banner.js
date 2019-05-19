ed02102b2c04ae0673c17bf4d79e14cbca8da5d7
quanghuy1242 <katygaga124@gmail.com>
2019-05-19 12:48:16 +0700

    updated: more powerfull banner component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { Text } from 'office-ui-fabric-react/lib/Text';
-import { style } from './Banner.style';
+import { getStyle } from './Banner.style';
+import { css } from '@uifabric/utilities';
 
 class Banner extends Component {
   render() {
+    const classNames = getStyle(this.props);
     return (
-      <div className={style.bannerWrapper}>
-        <div style={{ textAlign: 'center' }}>
-          <Text variant="superLarge" className={style.headerText}>
+      <div className={classNames.bannerWrapper}>
+        <div className={classNames.textWrapper}>
+          <Text variant="superLarge" className={classNames.headerText}>
             {this.props.title}
           </Text>
-          <Text variant="xLarge" className={style.headerText}>
+          <Text variant="xLarge" className={classNames.headerText}>
             {this.props.subTitle}
           </Text>
+          {this.props.children}
         </div>
+        {this.props.hasOverlay && <div className={css(classNames.overlay, 'overlay')}></div>}
       </div>
     );
   }
--- src/components/Banner/Banner.style.js
+++ src/components/Banner/Banner.style.js
@@ -1,17 +1,36 @@
 import { mergeStyleSets } from 'office-ui-fabric-react';
 
-export const style = mergeStyleSets({
-  bannerWrapper: {
-    width: '100%',
-    height: 250,
-    backgroundColor: 'rgb(0, 120, 212)',
-    display: 'grid',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  headerText: {
-    color: 'white',
-    display: 'block',
-    marginBottom: '0.5rem'
-  }
-})
+export const getStyle = ({ image, height }) => {
+  return mergeStyleSets({
+    bannerWrapper: {
+      width: '100%',
+      height: height || 250,
+      ...(image) && { backgroundImage: `url(${image})` },
+      ...(!image) && { backgroundColor: 'rgb(0, 120, 212)' },
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center',
+      display: 'grid',
+      alignItems: 'center',
+      justifyContent: 'center',
+      position: 'relative'
+    },
+    textWrapper: {
+      textAlign: 'center', 
+      zIndex: 20
+    },
+    headerText: {
+      color: 'white',
+      display: 'block',
+      marginBottom: '0.5rem'
+    },
+    overlay: {
+      backgroundColor: 'black',
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      opacity: 0.5
+    }
+  })
+}
