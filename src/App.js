02bc5eda62331a6a9c85994cfd2f4e9f9b300a38
quanghuy1242 <katygaga124@gmail.com>
2019-06-21 21:38:45 +0700

    migration: 6 -> 7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { Customizer } from 'office-ui-fabric-react';
-import { FluentCustomizations } from '@uifabric/fluent-theme';
 import MainApp from './components/MainApp/MainApp';
 import MainContent from './components/MainContent/MainContent';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -11,26 +9,24 @@ import { Helmet } from 'react-helmet';
 class App extends Component {
   render() {
     return (
-      <Customizer {...FluentCustomizations}>
-        <AppContextProvider>
-          <Router>
-            <MainApp>
-              <Helmet>
-                <title>Project Showcase!</title>
-                <meta property="fb:app_id" content="535340733663959" />
-                <meta name="description" content="Project showcase" />
-                <meta property="og:type" content="article" />
-                <meta name="twitter:site" content="@corysmc" />
-                <meta property="og:url" content="https://project-showcase.netlify.com/" />
-                <meta property="og:site_name" content="Quang Huy" />
-              </Helmet>
-              <MainContent>
-                <RouteContainer />
-              </MainContent>
-            </MainApp>
-          </Router>
-        </AppContextProvider>
-      </Customizer>
+      <AppContextProvider>
+        <Router>
+          <MainApp>
+            <Helmet>
+              <title>Project Showcase!</title>
+              <meta property="fb:app_id" content="535340733663959" />
+              <meta name="description" content="Project showcase" />
+              <meta property="og:type" content="article" />
+              <meta name="twitter:site" content="@corysmc" />
+              <meta property="og:url" content="https://project-showcase.netlify.com/" />
+              <meta property="og:site_name" content="Quang Huy" />
+            </Helmet>
+            <MainContent>
+              <RouteContainer />
+            </MainContent>
+          </MainApp>
+        </Router>
+      </AppContextProvider>
     );
   }
 }
