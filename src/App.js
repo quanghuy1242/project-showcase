bc110c690cad00906f0a06e529f5a6498d21f379
quanghuy1242 <katygaga124@gmail.com>
2019-06-24 09:45:31 +0700

    changed: wrap application with Fabic tag

--- src/App.js
+++ src/App.js
@@ -5,28 +5,31 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import RouteContainer from './components/RouteContainer/RouteContainer';
 import AppContextProvider from './context/AppContextProvider';
 import { Helmet } from 'react-helmet';
+import { Fabric } from 'office-ui-fabric-react';
 
 class App extends Component {
   render() {
     return (
-      <AppContextProvider>
-        <Router>
-          <MainApp>
-            <Helmet>
-              <title>Project Showcase!</title>
-              <meta property="fb:app_id" content="535340733663959" />
-              <meta name="description" content="Project showcase" />
-              <meta property="og:type" content="article" />
-              <meta name="twitter:site" content="@corysmc" />
-              <meta property="og:url" content="https://project-showcase.netlify.com/" />
-              <meta property="og:site_name" content="Quang Huy" />
-            </Helmet>
-            <MainContent>
-              <RouteContainer />
-            </MainContent>
-          </MainApp>
-        </Router>
-      </AppContextProvider>
+      <Fabric>
+        <AppContextProvider>
+          <Router>
+            <MainApp>
+              <Helmet>
+                <title>Project Showcase!</title>
+                <meta property="fb:app_id" content="535340733663959" />
+                <meta name="description" content="Project showcase" />
+                <meta property="og:type" content="article" />
+                <meta name="twitter:site" content="@corysmc" />
+                <meta property="og:url" content="https://project-showcase.netlify.com/" />
+                <meta property="og:site_name" content="Quang Huy" />
+              </Helmet>
+              <MainContent>
+                <RouteContainer />
+              </MainContent>
+            </MainApp>
+          </Router>
+        </AppContextProvider>
+      </Fabric>
     );
   }
 }
