f95fc4c1f6dfe6f8d1cbb4921cdacd1060a8421f
quanghuy1242 <quanghuy1242@gmail.com>
2019-07-28 16:45:22 +0700

    lazy loading

--- src/components/RouteContainer/RouteContainer.js
+++ src/components/RouteContainer/RouteContainer.js
@@ -1,23 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Home from '../../pages/Home/Home';
-import NotFound from '../../pages/NotFound/NotFound';
-import About from '../../pages/About/About';
-import ProjectDetail from '../../pages/ProjectDetail/ProjectDetail';
-import Categories from '../../pages/Categories/Categories';
-import CategoryDetail from '../../pages/CategoryDetail/CategoryDetail';
+const Home = lazy(() => import('../../pages/Home/Home'));
+const NotFound = lazy(() => import('../../pages/NotFound/NotFound'));
+const About = lazy(() => import('../../pages/About/About'));
+const ProjectDetail = lazy(() => import('../../pages/ProjectDetail/ProjectDetail'));
+const Categories = lazy(() => import('../../pages/Categories/Categories'));
+const CategoryDetail = lazy(() => import('../../pages/CategoryDetail/CategoryDetail'));
 
 class RouteContainer extends Component {
   render() {
     return (
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/project/:id" component={ProjectDetail} />
-        <Route path="/categories" exact component={Categories} />
-        <Route path="/categories/:nameId" component={CategoryDetail} />
-        <Route path="/about" component={About} />
-        <Route component={NotFound} />
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/project/:id" component={ProjectDetail} />
+          <Route path="/categories" exact component={Categories} />
+          <Route path="/categories/:nameId" component={CategoryDetail} />
+          <Route path="/about" component={About} />
+          <Route component={NotFound} />
+        </Switch>
+      </Suspense>
     );
   }
 }
