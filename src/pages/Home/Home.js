ee2c0b162810bd34f00efe4d903380eee70958cb
quanghuy1242 <quanghuy1242@gmail.com>
2019-08-23 19:10:58 +0700

    update home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,32 +1,50 @@
 import React, { Component } from 'react';
 import { Text } from 'office-ui-fabric-react/lib/Text';
 import { getStyle } from './Home.style';
-import { PrimaryButton } from 'office-ui-fabric-react';
+import { PrimaryButton, Stack, ImageFit, Image, css } from 'office-ui-fabric-react';
 import { withRouter } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+import MainContent from '../../components/MainContent/MainContent';
 
 class Home extends Component {
   render() {
     const classNames = getStyle();
     return (
-      <div className={classNames.homeWrapper}>
+      <Stack className={classNames.homeWrapper}>
         <Helmet>
           <title>Home - Project Showcase</title>
         </Helmet>
-        <Text
-          variant="superLarge" 
-          className={classNames.headerText}
-          block
-        >
-          COMING SOON
-        </Text>
-        <Text variant="large" block>Please check back later!</Text>
-        <PrimaryButton
-          text="Explore projects"
-          style={{ marginTop: '0.5rem' }}
-          onClick={() => {this.props.history.push('/projects')}}
-        />
-      </div>
+        <div className={classNames.imageWrapper}>
+          <Image
+            src="https://raw.githubusercontent.com/quanghuy1242/MyLibary/master/images/2019/5/20/photo-1517511620798-cec17d428bc0.jpg"
+            alt="image"
+            height={250}
+            imageFit={ImageFit.cover}
+            className={classNames.image}
+          />
+          <Stack className={css("detail")} horizontalAlign="center" verticalAlign="center">
+            <Text variant="superLarge" className={classNames.bannerText}>Project Showcase</Text>
+            <Text variant="xLarge" className={classNames.bannerText}>How my own world begins</Text>
+          </Stack>
+        </div>
+        <MainContent isChild={true}>
+          <Stack verticalAlign='center' horizontalAlign='center' style={{ marginTop: '1rem' }}>
+            <Text
+              variant="superLarge" 
+              className={classNames.headerText}
+              block
+            >
+              COMING SOON
+            </Text>
+            <Text variant="large" block>Please check back later!</Text>
+            <PrimaryButton
+              text="Explore projects"
+              style={{ marginTop: '0.5rem' }}
+              onClick={() => {this.props.history.push('/projects')}}
+            />
+          </Stack>
+        </MainContent>
+      </Stack>
     );
   }
 }
