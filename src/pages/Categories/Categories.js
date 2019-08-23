aa3fc758e49bdb774db39e1fd3d04e713072aff7
quanghuy1242 <quanghuy1242@gmail.com>
2019-08-23 19:00:06 +0700

    added banner to category page

--- src/pages/Categories/Categories.js
+++ src/pages/Categories/Categories.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import MainContent from '../../components/MainContent/MainContent';
-import { Text } from 'office-ui-fabric-react';
+import { Text, Stack, Image, css, ImageFit } from 'office-ui-fabric-react';
 import StackPanel from '../../components/StackPanel/StackPanel';
 import CategorieCollection from '../../components/CategoryCollection/CategoryCollection';
 import { getStyle } from './Categories.style';
@@ -39,31 +39,45 @@ class Categories extends Component {
   render() {
     const classNames = getStyle();
     return (
-      <MainContent hasPadding={true} isChild={true} className={classNames.categoryWrapper}>
-        <Helmet>
-          <title>{this.state.seoData.title}</title>
-          <meta name="keywords" content={this.state.seoData.title} />
-          <meta name="description" content={this.state.seoData.description} />
-          <meta property="og:title" content={this.state.seoData.title} />
-          <meta property="og:url" content={this.state.seoData.url} />
-          <meta property="og:image" content={this.state.seoData.image} />
-          <meta property="og:image:alt" content={this.state.seoData.description} />
-          <meta property="og:description" content={this.state.seoData.description} />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content={this.state.seoData.title} />
-          <meta name="twitter:text:title" content={this.state.seoData.title} />
-          <meta name="twitter:image" content={this.state.seoData.image} />
-          <meta name="twitter:image:alt" content={this.state.seoData.description} />
-          <meta name="twitter:description" content={this.state.seoData.description} />
-        </Helmet>
-        <StackPanel>
-          <Text variant="xLarge" className={classNames.headerText}>Categories</Text>
-          <CategorieCollection 
-            categoriesList={this.state.categories}
-            isLoading={this.state.isLoading}
+      <Stack className={classNames.categoryWrapper}>
+        <div className={classNames.imageWrapper}>
+          <Image
+            src={this.state.seoData.image}
+            alt="image"
+            height={250}
+            imageFit={ImageFit.cover}
+            className={classNames.image}
           />
-        </StackPanel>
-      </MainContent>
+          <Stack className={css("detail")} horizontalAlign="center" verticalAlign="center">
+            <Text variant="superLarge" className={css(classNames.headerText, classNames.whiteText)}>Categories</Text>
+          </Stack>
+        </div>
+        <MainContent isChild={true}>
+          <Helmet>
+            <title>{this.state.seoData.title}</title>
+            <meta name="keywords" content={this.state.seoData.title} />
+            <meta name="description" content={this.state.seoData.description} />
+            <meta property="og:title" content={this.state.seoData.title} />
+            <meta property="og:url" content={this.state.seoData.url} />
+            <meta property="og:image" content={this.state.seoData.image} />
+            <meta property="og:image:alt" content={this.state.seoData.description} />
+            <meta property="og:description" content={this.state.seoData.description} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={this.state.seoData.title} />
+            <meta name="twitter:text:title" content={this.state.seoData.title} />
+            <meta name="twitter:image" content={this.state.seoData.image} />
+            <meta name="twitter:image:alt" content={this.state.seoData.description} />
+            <meta name="twitter:description" content={this.state.seoData.description} />
+          </Helmet>
+          <StackPanel>
+            <Text variant="xLarge" className={classNames.headerText}>Categories</Text>
+            <CategorieCollection 
+              categoriesList={this.state.categories}
+              isLoading={this.state.isLoading}
+            />
+          </StackPanel>
+        </MainContent>
+      </Stack>
     );
   }
 }
--- src/pages/Categories/Categories.style.js
+++ src/pages/Categories/Categories.style.js
@@ -1,14 +1,37 @@
 import { mergeStyleSets } from 'office-ui-fabric-react';
-import { MotionAnimations, MotionDurations } from '@uifabric/fluent-theme';
+import { MotionAnimations, MotionDurations, Depths } from '@uifabric/fluent-theme';
 
 export const getStyle = () => {
   return mergeStyleSets({
     categoryWrapper: {
+      margin: '1rem',
       animation: MotionAnimations.slideUpIn,
       animationDuration: MotionDurations.duration4
     },
     headerText: {
       marginBottom: '0.5rem'
+    },
+    imageWrapper: {
+      position: 'relative',
+      marginBottom: '0.5rem',
+      selectors: {
+        '& .detail': {
+          position: 'absolute',
+          left: 0,
+          bottom: 0,
+          top: 0,
+          right: 0,
+          backgroundColor: 'rgba(0, 0, 0, 0.2)',
+          padding: '1rem'
+        }
+      }
+    },
+    image: {
+      boxShadow: Depths.depth8,
+      borderRadius: 2
+    },
+    whiteText: {
+      color: 'white'
     }
   })
 }
