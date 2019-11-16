b6284c09b66788f4a2f30c1d080b1ed2597e87d5
Quang Huy <quanghuy1242@gmail.com>
2019-11-16 13:32:24 +0700

    fixed layout on mobile

--- src/components/CategoryCollection/CategoryCollection.style.js
+++ src/components/CategoryCollection/CategoryCollection.style.js
@@ -7,7 +7,7 @@ export const getStyle = () => {
     },
     categoriesListWrapper: {
       display: 'grid',
-      gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+      gridTemplateColumns: 'repeat(auto-fill, minmax(280px, 1fr))',
       gridGap: '1rem',
       justifyItems: 'center'
     }
